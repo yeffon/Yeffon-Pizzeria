878ad8110477d7ff91f24ad2b8820d740076f4fb
yeffon <exdsxt@gmail.com>
2017-12-20 17:05:31 -0500

    Starting files made, created classes with constructors and destructors for each

---
+++ YeffonPizzeria/YeffonPizzeria/storeClasses.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef storeClasses
+#define storeClasses
+
+class Pizza
+{
+public:
+	Pizza();
+	~Pizza();
+};
+
+class Wings
+{
+public:
+	Wings();
+	~Wings();
+};
+
+class Desserts
+{
+public:
+	Desserts();
+	~Desserts();
+};
+#endif // DEBUG
+
